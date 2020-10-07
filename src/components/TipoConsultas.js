387c9def6f7f6571dffb0adf38b1e99adac3d429
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-10-07 01:13:39 -0300

    Criação do componente TipoConsultas
    Criação do componente para opção de tipos de consulta.

---
+++ src/components/TipoConsultas.js
@@ -0,0 +1,56 @@
+import React, {Component, useState} from 'react'
+import {Text, View, StyleSheet} from "react-native"
+import {RadioButton} from "react-native-paper"
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+
+
+const TipoConsultas = () => {
+    const [escolha, setEscolha] = useState("Gratuita")
+    
+    return (
+        <RadioButton.Group onValueChange={value => setEscolha(value)} value={escolha}>
+            <View style={styles.areaEscolha}>
+                <View style={styles.escolhaGratuita}>
+                    <RadioButton value="Gratuita" color="#34C5A2"/>
+                    <Text style={styles.txtGratuita}>Gratuita</Text>
+                </View>
+                <View style={styles.escolhaPaga} >
+                    <RadioButton value="Paga" color="#34C5A2"/>
+                    <Text style={styles.txtPaga}>Paga</Text>
+                </View>
+            </View>
+        </RadioButton.Group>
+    )
+}
+
+export default TipoConsultas
+
+const styles = StyleSheet.create({
+    areaEscolha: {
+        flexDirection: "row",
+        marginTop: hp("3%"),
+        borderStyle: "solid",
+        justifyContent: "space-around",
+        borderWidth: wp("0.8%"),
+        borderColor: "#80C6F9",
+        borderRadius: 10,
+        padding: 6,
+    },
+    escolhaGratuita: {
+        flexDirection: "row",
+        alignItems: "center",
+        fontSize: hp("2%"),
+    },
+    txtGratuita: {
+        fontSize: hp("2.7%"),
+        fontWeight: "bold",
+    },
+    escolhaPaga: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    txtPaga: {
+        fontSize: hp("2.7%"),
+        fontWeight: "bold",
+    }
+})
