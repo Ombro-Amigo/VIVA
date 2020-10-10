27695e5668998642a4bc7fe0f5df05bc1d21a9f1
Natan Abreu <guilhermenatanabreu@gmail.com>
2020-10-09 23:47:04 -0300

    Começando a estilização da página confirmação de CRP

--- src/pages/psicologo/CadastroPsicologo1.js
+++ src/pages/psicologo/CadastroPsicologo1.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 export default function CadastroPsicologo1() {
    return (
--- src/pages/psicologo/ConfirmacaoCrp.js
+++ src/pages/psicologo/ConfirmacaoCrp.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Fundo from '../../components/Fundo'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import Botao from '../../components/Botao'
 
 export default function ConfirmacaoCrp() {
    return (
-      <View>
-         <Text>ConfirmacaoCrp</Text>
-      </View>
+      <Fundo>
+         <View>
+            <Text style={[styles.title, styles.txtCor]}>Quase lá!</Text>
+            <Text>Seus dados foram registrados!</Text>
+         </View>
+         <Botao title='Concluir' style={styles.btn}/>
+      </Fundo>
+      
    )
 }
+
+const styles = StyleSheet.create({
+   title: {
+      fontWeight: 'bold',
+      fontSize: wp('15%'),
+      textAlign: 'center',
+   },
+   txtCor: {
+      color: '#186794',
+   },
+   btn: {
+      paddingHorizontal: wp('5%'),
+      paddingVertical: hp('5%'),
+      
+   }
+})
