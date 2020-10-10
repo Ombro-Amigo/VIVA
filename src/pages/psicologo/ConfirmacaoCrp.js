7416e494bc7f8ad86ee36bde223abcedcd1ec65e
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-10-10 00:53:09 -0300

    tela crp

--- src/pages/psicologo/ConfirmacaoCrp.js
+++ src/pages/psicologo/ConfirmacaoCrp.js
@@ -1,34 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import Fundo from '../../components/Fundo'
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import Botao from '../../components/Botao'
+import { View, Text } from 'react-native'
 
 export default function ConfirmacaoCrp() {
    return (
-      <Fundo>
-         <View>
-            <Text style={[styles.title, styles.txtCor]}>Quase lá!</Text>
-            <Text>Seus dados foram registrados!</Text>
-         </View>
-         <Botao title='Concluir' style={styles.btn}/>
-      </Fundo>
-      
+      <View>
+         <Text>ConfirmacaoCrp</Text>
+      </View>
    )
 }
-
-const styles = StyleSheet.create({
-   title: {
-      fontWeight: 'bold',
-      fontSize: wp('15%'),
-      textAlign: 'center',
-   },
-   txtCor: {
-      color: '#186794',
-   },
-   btn: {
-      paddingHorizontal: wp('5%'),
-      paddingVertical: hp('5%'),
-      
-   }
-})
