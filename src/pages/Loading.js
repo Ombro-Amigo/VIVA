ac24f75ec219ef6546802fa5f3c1f0d08611dfce
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-10-21 16:09:55 -0300

    criando componente loading

---
+++ src/pages/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+
+export default function Loading() {
+   return (
+      <View style={styles.container}>
+         <ActivityIndicator size='large' color='#34C5A2' />
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: 'center'
+   }
+})
