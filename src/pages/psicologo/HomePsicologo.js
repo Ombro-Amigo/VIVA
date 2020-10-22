c71133a97530dca8aa5c4e12cddfd9f4ede44421
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-10-22 05:08:07 -0300

    Adicionando o navigation

--- src/pages/psicologo/HomePsicologo.js
+++ src/pages/psicologo/HomePsicologo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function HomePsicologo() {
+export default function HomePsicologo({navigation}) {
    return (
       <View>
          <Text>HomePsicologo</Text>
