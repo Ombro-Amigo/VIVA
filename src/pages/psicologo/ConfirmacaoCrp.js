b9e023e1c792765c8c324a11e55ffb502ac74e22
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-10-23 16:07:16 -0300

    Codificação da tela de ConfirmacaoCrp

--- src/pages/psicologo/ConfirmacaoCrp.js
+++ src/pages/psicologo/ConfirmacaoCrp.js
@@ -1,10 +1,94 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Fundo from '../../components/Fundo'
+import Botao from '../../components/Botao'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function ConfirmacaoCrp({navigation}) {
+
+   const [validade, setValidade] = useState(true)
+
    return (
-      <View>
-         <Text>ConfirmacaoCrp</Text>
-      </View>
+      <ScrollView style={styles.container}>
+         <Fundo>
+            <View style={styles.titleContainer}>
+               <Text style={styles.title}>Quase lá</Text>
+               <Text style={styles.subTitle}>Seus dados foram registrados</Text>
+            </View>
+
+            <View style={styles.mensagemContainer}>
+               <Text style={styles.mensagem}>Agora precisamos que aguarde
+                     enquanto o processo de validação
+                     do seu CRP é efetuado.
+               </Text>
+            </View>
+               
+            <View style={styles.areaSituacaoCrp}>
+               <Text style={styles.exibeCrp}>Seu CRP: XX/XXXXXX</Text>
+               <Text style={styles.exibeSituacaoValidacao}>
+                  Situação da validação:
+                  <Text style={styles.status}> Status</Text>
+               </Text>
+            </View>
+
+            <Botao 
+               corFundo={validade ? "#34C5A2" : "#C3C3C3"}
+               style={styles.btn}
+               title="Concluir"
+               desabilitado={!validade}
+            />
+
+         </Fundo>
+      </ScrollView>
    )
 }
+
+const styles = StyleSheet.create({
+   container: {
+      backgroundColor: "#6EB4E7",
+   },
+   title: {
+      color: "#186794",
+      marginTop: hp("3%"),
+      fontSize: wp("8%"),
+      fontWeight: "bold",
+      alignSelf: "center",
+   },
+   subTitle: {
+      color: "#186794",
+      marginTop: hp("3%"),
+      fontSize: wp("7%"),
+      fontWeight: "bold", 
+      textAlign: "center"
+   },
+   mensagem: {
+      color: "#FFF",
+      marginTop: hp("8%"),
+      paddingHorizontal: wp("2%"),
+      fontSize: wp("4.7%"),
+      fontWeight: "bold",
+      textAlign: "justify",
+   },
+   exibeCrp: {
+      color: "#186794",
+      marginTop: hp("10%"),
+      fontSize: wp("6.1%"),
+      fontWeight: "bold",
+      alignSelf: "center"
+   },
+   exibeSituacaoValidacao: {
+      color: "#000",
+      marginTop: hp("3%"),
+      fontSize: wp("5%"),
+      fontWeight: "bold",
+      alignSelf: "center",
+   },
+   status: {
+      color: "#FF8000",
+   },
+   btn: {
+      marginTop: hp("6%"),
+      paddingVertical: hp("2.5%"),
+   }
+})
