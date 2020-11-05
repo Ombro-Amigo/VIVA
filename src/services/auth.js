a9c010445e94aafe3156dc6357108941fc4480b6
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-11-05 14:48:37 -0300

    Implementação de autenticação com o Facebook

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,8 @@
 import auth from '@react-native-firebase/auth';
-import { confirmTypeUser } from './database';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
 
 export const login = async (email, senha) => {
-  await auth()
+  auth()
     .signInWithEmailAndPassword(email, senha)
     .then(userInfo => {
       console.log('logou!');
@@ -12,8 +12,30 @@ export const login = async (email, senha) => {
     });
 }
 
+export const loginWithFacebook = async () => {
+  // Attempt login with permissions
+  const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+  if (result.isCancelled) {
+    throw 'User cancelled the login process';
+  }
+
+  // Once signed in, get the users AccesToken
+  const data = await AccessToken.getCurrentAccessToken();
+
+  if (!data) {
+    throw 'Something went wrong obtaining access token';
+  }
+
+  // Create a Firebase credential with the AccessToken
+  const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+  // Sign-in the user with the credential
+  auth().signInWithCredential(facebookCredential);
+}
+
 export const signIn = async (email, senha) => {
-  await auth()
+  auth()
     .createUserWithEmailAndPassword(email, senha)
     .then(() => {
       console.log('inscreveu!');
@@ -24,7 +46,7 @@ export const signIn = async (email, senha) => {
 }
 
 export const signOut = async () => {
-  await auth()
+  auth()
     .signOut()
     .then(() => console.log('Saiu!'))
     .catch(error => {
