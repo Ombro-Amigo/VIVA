da7a3f83526a2685ff3e8412cb0bdf4a462cd22b
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-11-05 20:58:58 -0300

    Refatoração do componente.
    Remoção do objeto com as informações de consultas e implementação do componente Card

--- src/components/ListaConsultas.js
+++ src/components/ListaConsultas.js
@@ -1,70 +1,34 @@
-import React, { Component} from "react"
-import { View, Image, Text, StyleSheet, FlatList } from "react-native"
-import { TouchableOpacity } from "react-native-gesture-handler"
+import React from "react"
+import { StyleSheet, FlatList } from "react-native"
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
-import { Divider } from 'react-native-paper'
-import Botao from './Botao'
 import Card from './Card'
+import consultasPaciente from '../utils/consultasPaciente.json'
+import consultasPsicologo from '../utils/consultasPsicologo.json'
 
-export default class ListaConsultas extends Component{
-    render(){
-        const consultas = [
-            {
-                id: "1",
-                name: "Nome do(a) psicólogo(a)",
-                photo: require("../assets/icon/usuario-cards-e-menu.png"),
-                date: "dd/mm/aaaa",
-                start: "hh:mm",
-                end: "hh:mm",
-                status: "Confirmada"
-            },
-            {
-                id: "2",
-                name: "Nome do(a) psicólogo(a)",
-                photo: require("../assets/icon/usuario-cards-e-menu.png"),
-                date: "dd/mm/aaaa",
-                start: "hh:mm",
-                end: "hh:mm",
-                status: "Confirmada"
-            },
-            {
-                id: "3",
-                name: "Nome do(a) psicólogo(a)",
-                photo: require("../assets/icon/usuario-cards-e-menu.png"),
-                date: "dd/mm/aaaa",
-                start: "hh:mm",
-                end: "hh:mm",
-                status: "Confirmada"
-            },
-            {
-                id: "4",
-                name: "Nome do(a) psicólogo(a)",
-                photo: require("../assets/icon/usuario-cards-e-menu.png"),
-                date: "dd/mm/aaaa",
-                start: "hh:mm",
-                end: "hh:mm",
-                status: "Confirmada"
+export default function ListaConsultas(props){
+    const consultasPac = consultasPaciente.consultas
+    const consultasPsico = consultasPsicologo.consultas
+
+    const { usuario } = props
+
+    return(
+        <FlatList
+            style={styles.flatList}
+            data={usuario === "pc" ? consultasPac : consultasPsico}
+            renderItem={({item}) => 
+                <Card 
+                    name={item.name}
+                    date={item.date}
+                    crp={item.crp}
+                    start={item.start}
+                    end={item.end}
+                    status={item.status}
+                /> 
             }
-        ]
-
-        return(
-            <FlatList
-                style={styles.flatList}
-                data={consultas}
-                renderItem={({item}) => 
-                    <Card 
-                        photo={item.photo}
-                        name={item.name}
-                        date={item.date}
-                        start={item.start}
-                        end={item.end}
-                        status={item.status}
-                    /> 
-                }
-                keyExtractor={item => item.id}
-            />
-        )
-    }
+            keyExtractor={item => item.id}
+        />
+    )
+    
 }
 
 const styles = StyleSheet.create({
@@ -73,6 +37,7 @@ const styles = StyleSheet.create({
         height: hp("52%"),
         borderRadius: 10,
         borderStyle: 'solid',
-        padding: 15,
+        paddingHorizontal: wp("4%"),
+        paddingVertical: hp("1%"),
     },
 });
