25700b9a1728af8a26428fef1ad51b8e6a12b193
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-11-10 07:49:01 -0300

    Adicionando as variáveis para controle de validação do CRP do psicólogo

--- src/contexts/states.js
+++ src/contexts/states.js
@@ -6,8 +6,13 @@ const StatesContext = createContext()
 export const StatesProvider = ({children}) => {
     const [dispo, setDispo] = useState(1)
     const [rangeConsultas, setRangeConsultas] = useState(2)
+    const [validated, setValidated] = useState(false)
     return(
-        <StatesContext.Provider value={{dispo, setDispo, rangeConsultas, setRangeConsultas}}>
+        <StatesContext.Provider value={{
+            dispo, setDispo, 
+            rangeConsultas, setRangeConsultas,
+            validated, setValidated,
+        }}>
             {children}
         </StatesContext.Provider>
     )
