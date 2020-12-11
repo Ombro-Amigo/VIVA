acdeb1c53990dfd8d7eb7e7942eab60d84d0e1dc
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-11 19:54:08 -0300

    Implementação da chamada do Modal de Construção

--- src/components/HeaderChamadas/index.js
+++ src/components/HeaderChamadas/index.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Divider } from 'react-native-paper';
 
+import ModalConstrucao from '../modalConstrucao';
 import styles from './style';
 
 function HeaderChamadas() {
+	const [modalVisible, setModalVisible] = useState(false);
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Solicitar chamada</Text>
-			<View style={styles.areaOpcoes}>
-				<TouchableOpacity>
-					<Image
-						source={require('../../assets/icon/telefone-com-sinal.png')}
-						style={styles.icon}
-					/>
-				</TouchableOpacity>
-				<Divider style={styles.divider}/>
-				<TouchableOpacity>
-					<Image
-						source={require('../../assets/icon/camera-video.png')}
-						style={styles.icon}
-					/>
-				</TouchableOpacity>
+		<>
+			<View style={styles.container}>
+				<Text style={styles.title}>Solicitar chamada</Text>
+				<View style={styles.areaOpcoes}>
+					<TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+						<Image
+							source={require('../../assets/icon/telefone-com-sinal.png')}
+							style={styles.icon}
+						/>
+					</TouchableOpacity>
+					<Divider style={styles.divider}/>
+					<TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+						<Image
+							source={require('../../assets/icon/camera-video.png')}
+							style={styles.icon}
+						/>
+					</TouchableOpacity>
+				</View>
 			</View>
-		</View>
+
+			<ModalConstrucao
+				modalVisible={modalVisible}
+				setModalVisible={setModalVisible}
+			/>
+		</>
 	);
 }
 
